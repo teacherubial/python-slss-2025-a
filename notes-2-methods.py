c3749bc36dd8b124ef404550b3dc4e321ba79e86
Tim Ubial <tim.ubial@gmail.com>
2025-10-07 14:16:18 -0700

    Added Methods notes

---
+++ notes-2-methods.py
@@ -0,0 +1,15 @@
+# Methods
+# Author: Ubial
+# 6 October
+
+# Ask the user what the weather is like
+weather = input("What's the weather like right now?")
+
+if weather.lower().strip("!,.?") == "rainy":
+    # "RAINY!"
+    print("You should bring an umbrella.")
+elif weather.lower().strip("!.,?") == "sunny":
+    # "SUNNY" "sunny!" "sunny."
+    print("You should bring sunscreen.")
+else:
+    print("I see...")
