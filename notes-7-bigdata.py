3cb9549df4cef364e2761e7da2b113d231d6cdfc
Tim Ubial <tim.ubial@gmail.com>
2025-11-19 13:58:15 -0800

    big data notes

--- notes-7-bigdata.py
+++ notes-7-bigdata.py
@@ -13,9 +13,33 @@ def main():
     # Read the file
     header_row = file.readline()
 
-    # Read the rest of the file
+    # Get information about the fave pizza place
+    uncle_fatihs = 0
+    club_ilia = 0
+    pizza_hut = 0
+
     for line in file:
-        print(line)
+        # fave pizza is in column 5 (4 index)
+        # each line is a string
+        # split the string wherever a , is
+        # convert from string -> list
+        info = line.split(",")
+        fave_pizza = info[4]
+
+        if fave_pizza == "Uncle Fatih's":
+            uncle_fatihs += 1
+        elif fave_pizza == "Club Ilia":
+            club_ilia += 1
+        elif fave_pizza == "Pizza Hut":
+            pizza_hut += 1
+
+    # Display results
+    print("------------------")
+    print("Results:")
+    print(f"Uncle Fatih's votes: {uncle_fatihs}")
+    print(f"Club Ilia votes: {club_ilia}")
+    print(f"Pizza Hut votes: {pizza_hut}")
+    print("------------------")
 
     file.close()
 
---
+++ work-mathsproblems.py
@@ -0,0 +1,20 @@
+# Maths Problems
+# Author: Ubial
+# 14 November
+
+# Solving Maths Problems
+
+def main():
+    # Question 1
+    current_age = int(input())
+
+    # Difference between this year and 2049
+    # Add the diff to current_age
+
+    # Question 2
+    #
+
+    # Question 3
+
+if __name__ == "__main__":
+    main()
