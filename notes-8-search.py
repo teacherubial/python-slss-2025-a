11aa0f3eaa1f1766bd958f1e2894204767702e57
Tim Ubial <tim.ubial@gmail.com>
2025-11-26 10:26:22 -0800

    search refined

--- notes-8-search.py
+++ notes-8-search.py
@@ -11,6 +11,7 @@ import csv
 
 def main():
     artist = "Kendrick Lamar" # artist to find
+    artist_two = "Justin Bieber"
     track_col = 0
     artist_col = 2
     yt_views_col = 11
@@ -33,6 +34,29 @@ def main():
 
         # print how many songs are in the list
         print(f"Number of Kendrick Songs: {len(kendrick_songs)}")
+
+        for song in kendrick_songs:
+            current_trackname = song[track_col]
+            current_ytviews = song[yt_views_col]
+            current_tiktokviews = song[tiktok_views_col]
+
+            # display information in a clear way
+            # Squabble Up       100,000,000     120,000,000
+            if 7 < len(current_trackname) < 14:
+                tabs = "\t\t\t"
+            elif 14 < len(current_trackname) < 18:
+                tabs = "\t\t"
+            elif 18 < len(current_trackname):
+                tabs = "\t"
+            else:
+                tabs = "\t\t\t\t"
+
+            print(f"{current_trackname.strip()}{tabs}{current_ytviews.strip()}\t{current_tiktokviews.strip()}")
+
+        # for song in kendrick_songs:
+        #     print(song)
+
+
 
 if __name__ == "__main__":
     main()
